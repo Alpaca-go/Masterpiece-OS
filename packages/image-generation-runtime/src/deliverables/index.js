0b0c727d2ef62bda0ed7647842206e76371f33d8
kyries1990 <kyries1990@gmail.com>
2026-07-27 23:26:29 +0800

    feat(image-generation): compile deliverable-specific prompts

--- packages/image-generation-runtime/src/deliverables/index.js
+++ packages/image-generation-runtime/src/deliverables/index.js
@@ -1,1 +1,2 @@
 export { DELIVERABLE_POLICIES, getDeliverablePolicy } from './deliverable-policies.js';
+export { resolveUserIntent, compileDeliverablePrompt } from './deliverable-prompt-compiler.js';
